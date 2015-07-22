61fded8095f39da69c1ca32abdaeed464cdc6fa8
shonenada <shonenada@gmail.com>
2015-07-23 00:17:40 +0800

    init project

---
+++ setup.py
@@ -0,0 +1,16 @@
+from setuptools import setup, find_packages
+
+
+install_required = [l.strip() for l in open('requestments.txt', 'r')]
+
+
+metadata = {
+    'name': '',
+    'version': '0.1',
+    'packages': find_packages(),
+    'author': 'shonenada',
+    'author_email': 'shonenada@gmail.com',
+    'install_requires': install_required,
+}
+
+setup(**metadata)
